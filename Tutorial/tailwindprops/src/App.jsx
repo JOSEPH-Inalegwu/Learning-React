a5f3f35a2aa763a30a2f58aff3917941b0a4fa2d
INALEGWU-Joseph <josephjonahinalegwu@gmail.com>
2024-12-26 00:57:48 +0100

    Setup

---
+++ Tutorial/tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-slate-900 text-slate-500 font-semibold text-4xl' >Vite + Tailwind</h1>
+    </>
+  )
+}
+
+export default App
