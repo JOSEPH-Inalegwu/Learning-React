9ee3ac39a91d7fe7d2b1edda89cedd1603f05c8e
INALEGWU-Joseph <josephjonahinalegwu@gmail.com>
2024-12-26 02:27:04 +0100

    Add Card component and user profiles to App.
    feat: Enhance component props handling

--- Tutorial/tailwindprops/src/App.jsx
+++ Tutorial/tailwindprops/src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
 
+  const Angela = {
+    username: 'Angela Gold',
+    post: 'Staff Engineer',
+    profile: "https://images.pexels.com/photos/4492217/pexels-photo-4492217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }
+
+  const Grace = {
+    username: 'Grace Lin',
+    post: 'Lead Developer',
+    profile: "https://images.pexels.com/photos/4474047/pexels-photo-4474047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }
+
+  const Tokyo = {
+    username: 'Tokyo Drift',
+    post: 'UI/UX Designer',
+    profile: "https://images.pexels.com/photos/5244065/pexels-photo-5244065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+
   return (
     <>
-      <h1 className='bg-slate-900 text-slate-500 font-semibold text-4xl' >Vite + Tailwind</h1>
+      <h1 className='bg-slate-800 text-slate-500 font-semibold text-4xl rounded-lg py-3' >Vite + Tailwind</h1>
+      <Card {...Angela}/>
+
+      <Card  {...Grace} />
+
+      <Card {...Tokyo} />
     </>
   )
 }
---
+++ Tutorial/tailwindprops/src/Components/Card.jsx
@@ -0,0 +1,42 @@
+
+import PropTypes from 'prop-types';
+
+function Card({
+  username = "John Doe",
+  post = "Not assigned", 
+  profile = "https://randomuser.me/api/portraits" 
+}) {
+  
+  return (
+    <div>
+      <figure className="md:flex mb-12 bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={profile} alt="" width="384" height="512" />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I&apos;ve seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              {username}
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              {post}
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  username: PropTypes.string,
+  post: PropTypes.string,
+  profile: PropTypes.image,
+};
+
+export default Card
